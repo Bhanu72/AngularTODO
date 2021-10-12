ec0e1e3c75a53d8499641e8fe918974647bc21ed
Bhanu72 <mleobhanu@gmail.com>
2021-10-12 23:17:38 +0530

    done

---
+++ src/app/app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log("Server listening on " + PORT);
+});
